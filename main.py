175d55ea4785d948db4282dfaf513643fa38ad6c
Tifelabs <boluwatifeolusegun018@gmail.com>
2024-12-17 01:14:42 -0400

    wip

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# Ask user for their name
+username = input("Enter your name: ")
 
-#Ask user name
+# Confirm the user's input
+confirmation = input(f"Are you sure your name is {username.upper()}? (yes or no): ")
+
+# Validating the user's response
+if confirmation.lower() == "yes":
+    print("WELCOME!")
+
+elif confirmation.lower() == "no":
+    print("Please re-enter your name.")
+
+else:
+    print("Invalid command! Please enter 'yes' or 'no'.")
+
+
+def welcome
